6edb720d02b8fd49fca3de1bfbbd20fd1ce50c41
Zinnurbek Ahrorov <a.zinnurbek@gmail.com>
2024-06-12 10:45:20 +0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void start(){
+        System.out.println("======= WELCOME TO LINKEDIN ========");
+
+        System.out.println("1. CONNECTION \n2. MESSAGING");
+    }
+}
